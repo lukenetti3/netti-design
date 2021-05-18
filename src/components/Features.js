9e98fca65cb30960151483bc57c6a13edda6b507
Luke Netti <lukenetti@hey.com>
2021-05-17 20:55:13 -0400

    Collected data for Features section

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,6 +1,38 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 
 const Features = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      prismicHomepage {
+        data {
+          fast_websites_icon {
+            url
+          }
+          fast_websites_text {
+            text
+          }
+          messaging_icon {
+            url
+          }
+          messaging_text {
+            text
+          }
+          seo_icon {
+            url
+          }
+          seo_text {
+            text
+          }
+        }
+      }
+    }
+  `)
+
+  const document = data.prismicHomepage.data
+
+  console.log(document)
+
   return (
     <section className='bg-primaryBlue text-white'>
       <h1>Hello Features page</h1>
--- src/components/slices/Services.js
+++ src/components/slices/Services.js
@@ -1,25 +1,24 @@
 import React from "react"
 import ServicesImage from "../../images/services_squiggly.svg"
 import ServiceCard from "../ServiceCard"
-import { FaDesktop } from "react-icons/fa"
+import styled from "styled-components"
 
 const Services = ({ slice }) => {
-  const servicesBackground = {
-    backgroundImage: `url(${ServicesImage}`,
-    backgroundRepeat: "no-repeat",
-    minHeight: "500px",
-    width: "81%",
-    margin: "0 auto 150px auto",
-  }
+  const ServiceWrapper = styled.div`
+    background-image: url(${ServicesImage});
+    background-repeat: no-repeat;
+    min-height: 500px;
+    width: 81%;
+    margin: 0 auto 150px auto;
+    @media (max-width: 900px) {
+      background-image: none;
+    }
+  `
 
   return (
     <section>
       <h1 className='text-center mt-16 text-blueText'>Our Services</h1>
-      <div
-        style={servicesBackground}
-        className='flex items-center'
-        id='service-media-query'
-      >
+      <ServiceWrapper className='flex items-center'>
         <div className='flex justify-content lg:flex-nowrap flex-wrap max-w-6xl mx-auto'>
           {slice.items.map((item) => (
             <>
@@ -27,7 +26,7 @@ const Services = ({ slice }) => {
             </>
           ))}
         </div>
-      </div>
+      </ServiceWrapper>
     </section>
   )
 }
